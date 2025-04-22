81fb494aee63bebe02649f010c661dbf675b683c
Alex Behm <112672371+abehm815@users.noreply.github.com>
2025-04-21 23:04:45 -0500

    Update ExampleUsage.java

--- src/cs3110/hw4/ExampleUsage.java
+++ src/cs3110/hw4/ExampleUsage.java
@@ -3,12 +3,12 @@ package cs3110.hw4;
 public class ExampleUsage {
 
 	public static void main(String[] args) {
-	//Show the before and after of the two examples in the resource folder
-        for (int i = 1; i <= 2; i++) {
-            String imagePath = "resources/example" + i + ".bmp";
-            System.out.println("Processing: " + imagePath);
-            CharacterSeparator.visualizeSeparations(imagePath);
-        }
+		//Show the before and after of the two examples in the resource folder
+	        for (int i = 1; i <= 2; i++) {
+	            String imagePath = "resources/example" + i + ".bmp";
+	            System.out.println("Processing: " + imagePath);
+	            CharacterSeparator.visualizeSeparations(imagePath);
+	        }
 
 	}
 
