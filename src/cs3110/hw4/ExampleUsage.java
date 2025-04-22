23fcc803b1cca6ac2b750c3dfea016420328a133
Alex Behm <112672371+abehm815@users.noreply.github.com>
2025-04-21 23:04:23 -0500

    Update ExampleUsage.java

--- src/cs3110/hw4/ExampleUsage.java
+++ src/cs3110/hw4/ExampleUsage.java
@@ -3,7 +3,7 @@ package cs3110.hw4;
 public class ExampleUsage {
 
 	public static void main(String[] args) {
-		// Loop through test1.bmp to test10.bmp in the resources folder to visually show where it sho
+	//Show the before and after of the two examples in the resource folder
         for (int i = 1; i <= 2; i++) {
             String imagePath = "resources/example" + i + ".bmp";
             System.out.println("Processing: " + imagePath);
